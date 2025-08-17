bc58f1e5ef1ef3c4e76a345704e23515a28dc3fc
Igor Druzhinin <152170719+UnBergant@users.noreply.github.com>
2025-08-17 10:10:59 +0200

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -5,7 +5,7 @@ dotenv.config();
 
 const getRequiredEnvVar = (key: string): string => {
     const value = process.env[key];
-    if (!value) throw new Error(`❌ Missing required env variable: ${key}`);
+    if (!value) throw new Error(`❌ Oops! Missing required env variable: ${key}`);
     return value;
 };
 
